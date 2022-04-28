b25da196f6b29a9893ae48204e99ac7f56c3ccb8
amirbasiri2000 <78272810+amirbasiri2000@users.noreply.github.com>
2022-04-28 11:45:34 +0430

    fgsfdg

---
+++ out/_next/static/Yo3l0pUwdjrqURVT-0vOk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-79f06249939ae3ce.js","static/chunks/940-33bec389eb8a2076.js","static/chunks/pages/index-aa1591b1a5a1cbb1.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
